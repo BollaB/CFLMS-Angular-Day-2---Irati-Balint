18c1b9027586de74ab0b24bde0f717bf104c8d3f
Irati Larreina <irati_6@hotmail.com>
2020-06-23 14:25:13 +0200

    adds challenge

--- AngularDay2/src/app/home/home.component.ts
+++ AngularDay2/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'home',
@@ -7,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+
 	cars = [
 {
 	name: "Lightning McQueen",
@@ -14,38 +18,53 @@ export class HomeComponent implements OnInit {
 	gender: "Male",
 	occupation: "Piston Cup Racer",
 	significant_other: "Sally Carrera",
-	nationality: "American",
-	source: "img/car1.png"
+	nationality: "American"
 },{
 	name: "Mater",
 	species: "1956-57 International Harvester boom truck",
 	gender: "Male",
 	occupation: "Owner of Tow Mater Towing & Salvage",
 	significant_other: "Holley Shiftwell",
-	nationality: "American",
-	source: "img/car2.png"
+	nationality: "American"
 },{
 	name: "Doc Hudson",
 	species: "Hudson Hornet motorcar",
 	gender: "Male",
 	occupation: "MD, judge, former racer, coach and mentor",
 	significant_other: "none",
-	nationality: "American",
-	source: "img/car3.png"
+	nationality: "American"
 },{
 	name: "Sally Carrera",
 	species: "Porsche 996 motorcar",
 	gender: "Female",
 	occupation: "town attorney, hotelier",
 	significant_other: "Lightning McQueen",
-	nationality: "American",
-	source: "img/car4.png"
+	nationality: "American"
 }
 	]	
+
+	info = new FormGroup({
+   name: new FormControl('',Validators.required),
+   species: new FormControl('',Validators.required),
+   gender: new FormControl('',Validators.required),
+   occupation:new FormControl('',Validators.required),
+   significant_other:new FormControl('',Validators.required),
+   nationality:new FormControl('',Validators.required),
+   
+ });
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  onSubmit(){
+   if(this.info.valid){
+   	var new_car = this.info.value;
+   	car.push(new_car)
+
+	}
+ }
 }
+
